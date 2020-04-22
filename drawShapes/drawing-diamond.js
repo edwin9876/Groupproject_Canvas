cecc05496e855d14feb1784c598c2cbefcbee2ca
YIN <anotherone@YINde-Air.lan>
2020-04-22 11:52:17 +0800

    diamond

---
+++ drawShapes/drawing-diamond.js
@@ -0,0 +1,43 @@
+class DrawDiamond extends PaintFunc{
+    constructor(ctxR,ctxD){
+        super();
+        this.ctxR = ctxR;
+        this.ctxD = ctxD;
+    }
+    onMd(coord,e) {
+
+    }
+   onDrag(coord,coord2,e) {
+    [coord2[0], coord2[1]] = [e.offsetX, e.offsetY]
+    ctxD.clearRect(0, 0, canvasD.width, canvasD.height)
+    ctxD.beginPath();
+    ctxD.moveTo(coord[0],coord[1]);
+    ctxD.lineTo(coord2[0],coord2[1]);
+    ctxD.lineTo(coord[0],(2*(coord2[1]-coord2[0])+coord2[0]));
+    ctxD.lineTo((2*(coord[0])-coord2[0]),coord2[1]);
+    ctxD.lineTo(coord[0],coord2[0]);
+    ctxD.stroke();
+    }
+   onMm(coord,e) {
+
+
+    }
+   onMu(coord,coord2,e) {
+    ctxD.clearRect(0, 0, canvasD.width, canvasD.height)
+    ctxR.beginPath();
+    ctxR.moveTo(coord[0],coord2[0]);
+    ctxR.lineTo(coord2[0],coord2[1]);
+    ctxR.lineTo(coord[0],(2*(coord2[1]-coord2[0])+coord2[0]));
+    ctxR.lineTo((2*(coord[0])-coord2[0]),coord2[1]);
+    ctxR.lineTo(coord[0],coord2[0]);
+    ctxR.stroke();
+    }
+   onMl(coord,e) {
+
+
+    }
+   onMe(coord,e) {
+
+
+    }
+}
